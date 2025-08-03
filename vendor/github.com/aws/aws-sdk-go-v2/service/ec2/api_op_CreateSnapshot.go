903f5da5fbd69463d7d701ccb24ab5b1c620076e
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2025-08-03 16:47:56 +0200

    [no-relnote] use aws-go-v2
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- vendor/github.com/aws/aws-sdk-go-v2/service/ec2/api_op_CreateSnapshot.go
+++ vendor/github.com/aws/aws-sdk-go-v2/service/ec2/api_op_CreateSnapshot.go
@@ -218,8 +218,7 @@ type CreateSnapshotOutput struct {
 	TransferType types.TransferType
 
 	// The ID of the volume that was used to create the snapshot. Snapshots created by
-	// a copy snapshot operation have an arbitrary volume ID that you should not use
-	// for any purpose.
+	// the CopySnapshotaction have an arbitrary volume ID that should not be used for any purpose.
 	VolumeId *string
 
 	// The size of the volume, in GiB.
