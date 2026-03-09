05e6dc9947920a8838a255ef7d4135ce4413011b
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2026-03-09 15:31:33 +0100

    feat: add ExpandPath utility for tilde expansion
    os.ReadFile does not expand ~ in paths. ExpandPath replaces a leading
    tilde with the user's home directory. Used for privateKey paths.
    
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

---
+++ pkg/utils/path.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// ExpandPath expands a leading ~ to the user's home directory.
+// Non-tilde paths are returned unchanged.
+func ExpandPath(path string) (string, error) {
+	if path == "" || !strings.HasPrefix(path, "~") {
+		return path, nil
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	if path == "~" {
+		return home, nil
+	}
+
+	// ~/foo/bar → /home/user/foo/bar
+	return filepath.Join(home, path[2:]), nil
+}
